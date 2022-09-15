7ee6657d928cc8261a98a1bb8c26298619242912
AqibChattha <aqibali543@gmail.com>
2022-09-16 01:51:16 +0500

    Project creation

---
+++ SmartBuilding/BuildingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBuilding
+{
+    class BuildingController
+    {
+        private string buildingId;
+        private string CurrentState;
+
+        public BuildingController(string id)
+        {
+            this.buildingId = id;
+        }
+
+        public BuildingController()
+        {
+            
+        }
+    }
+}
